50fd88ed43591b17286d3e27d7925ef527059c3b
CSELonewolf <devkantgorai1@gmail.com>
2022-02-21 20:32:33 +0530

    Display badges in the home page.

--- digital_badge/views.py
+++ digital_badge/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from badge.models import Badge
 
 def home(request):
-	return render(request, 'home.html')
+	badges = None
+	emails = []
+	badges = Badge.objects.all()
+	context = {
+		'badges':badges,
+	}
+	return render(request, 'home.html',context)
 
